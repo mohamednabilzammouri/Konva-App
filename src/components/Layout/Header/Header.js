93cbc10cf164a87177fc66e9a295f22f8389a84f
mohamednabilzammouri <81160478+mohamednabilzammouri@users.noreply.github.com>
2022-03-26 10:33:51 +0800

    squares background

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -3,9 +3,12 @@ import { Button } from "@material-ui/core";
 function Header() {
   return (
     <div className="header">
-      <Button style={{ color: "white", paddingLeft: "5%" }}>My Polygons</Button>
-      <Button style={{ color: "white", paddingLeft: "5%" }}>Save Work</Button>
-      <Button style={{ color: "white", paddingLeft: "5%" }}>New Polygon</Button>
+      <Button style={{ color: "white", paddingLeft: "3%" }}>New Polygon</Button>
+      <Button style={{ color: "white", paddingLeft: "3%" }}>My Polygons</Button>
+      <Button style={{ color: "white", paddingLeft: "3%" }}>
+        Save Polygon
+      </Button>
+      <Button style={{ color: "white", paddingLeft: "3%" }}>Reset</Button>
 
       <img
         src="https://hasty.ai/assets/images/hasty-logo.svg"
--- src/components/Polygon/Polygon.jsx
+++ src/components/Polygon/Polygon.jsx
@@ -15,29 +15,31 @@ function Polygon(props) {
   } = props.polygonData;
 
   return (
-    <Stage
-      width={window.innerWidth}
-      height={window.innerHeight * 0.85}
-      onMouseDown={handleClick}
-      onMouseMove={handleMouseMove}
-    >
-      <Layer>
-        <Line
-          points={flattenedPoints}
-          stroke="black"
-          strokeWidth={5}
-          closed={polygon.isFinished}
-        />
-        <DisplayRect
-          polygonProps={{
-            polygon,
-            handleMouseOverStartPoint,
-            handleMouseOutStartPoint,
-            handleDragMovePoint,
-          }}
-        />
-      </Layer>
-    </Stage>
+    <div className="background">
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight * 0.85}
+        onMouseDown={handleClick}
+        onMouseMove={handleMouseMove}
+      >
+        <Layer>
+          <Line
+            points={flattenedPoints}
+            stroke="black"
+            strokeWidth={5}
+            closed={polygon.isFinished}
+          />
+          <DisplayRect
+            polygonProps={{
+              polygon,
+              handleMouseOverStartPoint,
+              handleMouseOutStartPoint,
+              handleDragMovePoint,
+            }}
+          />
+        </Layer>
+      </Stage>
+    </div>
   );
 }
 
